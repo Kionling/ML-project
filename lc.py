bc668dcd05335e86dea6b50c8763fab0f66ea358
Daniel Jauregui <djvdjv209@gmail.com>
2024-06-29 12:52:27 -0700

    Preprocessing data and standardizing categorial values into numerical values for training data

--- lc.py
+++ lc.py
@@ -7,4 +7,11 @@ from sklearn.preprocessing import LabelEncoder
 
 data = pd.read_csv("./dataset/slc.csv")
 
-# print(data.describe())
+print(data.dtypes)
+
+for column in data.columns: 
+    if data[column].dytypes == object:
+        le = LabelEncoder()
+        data[column] = le.fit_transform(data[column])
+    else:
+        data[column].map({1:0, 2:1})
