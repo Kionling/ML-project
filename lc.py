271d5229697722694d26bb94aadf023ee72de692
Daniel Jauregui <djvdjv209@gmail.com>
2024-07-01 09:15:20 -0700

    Created instance of RFC with the parameter of 100 decision tree and a random seed of 42. Trained the model on X, y training data for pattern evaluation. Devloped a dataframe with the two columns feature and importance. As well as created a data frame that filters for patients in the dataset to only include patients that have lung cancer.

--- lc.py
+++ lc.py
@@ -24,3 +24,12 @@ X = data.drop('LUNG_CANCER', axis=1)
 y = data['LUNG_CANCER']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_classifier.fit(X_train, y_train)
+
+feature_importance = pd.DataFrame({'feature': X.columns, 'importance': rf_classifier.feature_importances_})
+feature_importance = feature_importance.sort_values('importance', ascending=False)
+
+lung_cancer_patients = data[data['LUNG_CANCER'] == 1]
+common_characteristics = lung_cancer_patients.mean().sort_values(ascending=False)
